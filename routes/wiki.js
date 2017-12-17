f1dc7b09eb0fd26110c50c4aa19966a36255ff18
rutvikhp <rutvikhp@gmail.com>
2017-12-17 10:24:11 -0600

    final done

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,46 +3,43 @@ let router = express.Router();
 let models = require('../models');
 let Page = models.Page;
 let User = models.User;
-// module.exports = () => {
-  router.get('/', function(req, res, next) {
-    res.redirect('/');
+
+router.get('/', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.post('/', function(req, res, next) {
+
+  User.findOrCreate({
+    where: {
+      email: req.body.author_email,
+      name: req.body.author_name
+    }
+  }).spread((foundUser,created) => {
+      let page = Page.build({
+        title: req.body.title,
+        content: req.body.page_content,
+        status: req.body.page_status,
+        authorId: foundUser.id
+      });
+      page.save().then((p) => {
+        res.redirect('/wiki/'+p.urlTitle);
+      });
   });
+});
 
-  router.post('/', function(req, res, next) {
+router.get('/add', function(req, res, next) {
+  res.render('addpage');
+});
 
-    User.findOrCreate({
-      where: {
-        email: req.body.author_email,
-        name: req.body.author_name
-      }
-    }).spread((foundUser,created) => {
-        let page = Page.build({
-          title: req.body.title,
-          content: req.body.page_content,
-          status: req.body.page_status,
-          authorId: foundUser.id
-        });
-        page.save().then((p) => {
-          res.redirect('/wiki/'+p.urlTitle);
-        });
-    });
+router.get('/:url', function(req, res, next) {
+    Page.findOne({
+    where: {
+      urlTitle: req.params.url
+    }
+  }).then((foundPage)=>{
+    res.render('wikipage',{foundPage:foundPage});
   });
+});
 
-  router.get('/add', function(req, res, next) {
-    res.render('addpage');
-  });
-
-  router.get('/:url', function(req, res, next) {
-      Page.findOne({
-      where: {
-        urlTitle: req.params.url
-      }
-    }).then((foundPage)=>{
-      res.render('wikipage',{foundPage:foundPage});
-      //res.json(foundPage);
-    });
-  });
-
-  module.exports = router;
-  // return router;
-// };
+module.exports = router;
